32c2f22060cba8d5e6a4a9ff886604c921b7a800
Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
2024-11-04 23:32:23 -0500

    feat: create vpc subnets
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,3 +7,47 @@ resource "aws_vpc" "flytrap_vpc" {
     Name = "flytrap-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {} # fetches AZs;
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.flytrap_vpc.id # id of vpc above
+  cidr_block              = var.public_subnet_cidr[0] # set IP range for the public subnet
+  availability_zone       = data.aws_availability_zones.available.names[0] # set AZ
+  map_public_ip_on_launch = true # all instances on public subnet get a public IP
+
+  tags = {
+    Name = "flytrap-public-subnet-a" # this shows up as the public subnet name in AWS
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.flytrap_vpc.id # id of vpc above
+  cidr_block              = var.public_subnet_cidr[1] # set IP range for the public subnet
+  availability_zone       = data.aws_availability_zones.available.names[1] # set AZ
+  map_public_ip_on_launch = true # all instances on public subnet get a public IP
+
+  tags = {
+    Name = "flytrap-public-subnet-b" # this shows up as the public subnet name in AWS
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.flytrap_vpc.id # id of vpc above
+  cidr_block        = var.private_subnet_cidr[0] # set IP range for the private subnet
+  availability_zone = data.aws_availability_zones.available.names[0] # set AZ
+
+  tags = {
+    Name = "flytrap-private-subnet-a" # this shows up as the private subnet name in AWS
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.flytrap_vpc.id # id of vpc above
+  cidr_block        = var.private_subnet_cidr[1] # set IP range for the private subnet
+  availability_zone = data.aws_availability_zones.available.names[1] # set AZ
+
+  tags = {
+    Name = "flytrap-private-subnet-b" # this shows up as the private subnet name in AWS
+  }
+}
