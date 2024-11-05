a6773f5707dba4b3369eb99e2fe3667d849f627f
Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
2024-11-04 22:59:25 -0500

    feat: create sqs_configure module to update sqs policy
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

---
+++ modules/sqs_configure/main.tf
@@ -0,0 +1,22 @@
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = var.sqs_queue_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+      Action = "SQS:SendMessage"
+      Resource = var.sqs_queue_arn
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = var.api_gateway_arn
+        }
+      }
+    }]
+  })
+}
+
+# add lambda config after lambda is provisioned
