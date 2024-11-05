0b01eb8ce69e5dcab969f78855647b89b0cc198a
Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
2024-11-04 22:04:56 -0500

    feat: request models and validators
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -51,3 +51,75 @@ resource "aws_api_gateway_resource" "promises" {
   parent_id   = aws_api_gateway_resource.api.id  # Link to the parent resource
   path_part   = var.promises_path
 }
+
+resource "aws_api_gateway_model" "errors_request_model" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "ErrorsPostRequestModel"
+  content_type = "application/json"
+  schema = jsonencode({
+    type = "object"
+      properties = {
+        data = {
+          type = "object"
+          properties = {
+            error = {
+              type = "object"
+              properties = {
+                name = { type = "string" }
+                message = { type = "string" }
+                stack = { type = "string" }
+              }
+              required = ["name", "message", "stack"]
+            }
+            handled = { type = "boolean" }
+            timestamp = { type = "string", format = "date-time" }
+            project_id = { type = "string" }
+          }
+          required = ["error", "handled", "timestamp", "project_id"]
+        }
+      }
+  })
+}
+
+resource "aws_api_gateway_request_validator" "errors_request_validator" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "ErrorsPostRequestValidator"
+  validate_request_body = true
+  validate_request_parameters = false
+}
+
+resource "aws_api_gateway_model" "promises_request_model" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "PromisesPostRequestModel"
+  content_type = "application/json"
+  schema = jsonencode({
+    type = "object"
+
+    properties = {
+      data = {
+        type = "object"
+        properties = {
+          value = {
+            oneOf = [
+              { type = "string" },
+              { type = "number" },
+              { type = "object" },
+              { type = "null" }
+            ]
+          }
+          handled = { type = "boolean" }
+          timestamp = { type = "string", format = "date-time" }
+          project_id = { type = "string" }
+        }
+        required = ["value", "handled", "timestamp", "project_id"]
+      }
+    }
+  })
+}
+
+resource "aws_api_gateway_request_validator" "promises_request_validator" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "PromisesPostRequestValidator"
+  validate_request_body = true
+  validate_request_parameters = false
+}
