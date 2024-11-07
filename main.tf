e63d7e67163f7e1842fae8dfbc767e5d4413584d
Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
2024-11-07 13:32:35 -0500

    fix: change sqs module name to sqs_provision
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -20,43 +20,43 @@ module "vpc" {
 }
 
 module "rds" {
-  source        = "./modules/rds"
-  vpc_id        = module.vpc.vpc_id
-  db_subnet_ids = module.vpc.private_subnet_ids
+  source              = "./modules/rds"
+  vpc_id              = module.vpc.vpc_id
+  db_subnet_ids       = module.vpc.private_subnet_ids
   private_subnet_cidr = module.vpc.private_subnet_cidr
 }
 
-module "sqs" {
-  source = "./modules/sqs"
+module "sqs_provision" {
+  source = "./modules/sqs_provision"
 }
 
 module "api_gateway" {
   source         = "./modules/api_gateway"
   region         = var.aws_region
   account_id     = data.aws_caller_identity.current.account_id
-  sqs_queue_name = module.sqs.sqs_queue_name
-  sqs_queue_arn  = module.sqs.sqs_queue_arn
+  sqs_queue_name = module.sqs_provision.sqs_queue_name
+  sqs_queue_arn  = module.sqs_provision.sqs_queue_arn
 }
 
 module "sqs_configure" {
   source          = "./modules/sqs_configure"
   api_gateway_arn = module.api_gateway.api_gateway_execution_arn
-  sqs_queue_arn   = module.sqs.sqs_queue_arn
-  sqs_queue_name  = module.sqs.sqs_queue_name
-  sqs_queue_id    = module.sqs.sqs_queue_id
+  sqs_queue_arn   = module.sqs_provision.sqs_queue_arn
+  sqs_queue_name  = module.sqs_provision.sqs_queue_name
+  sqs_queue_id    = module.sqs_provision.sqs_queue_id
 }
 
 module "lambda" {
-  source = "./modules/lambda"
-  vpc_id = module.vpc.vpc_id
-  db_secret_name = module.rds.db_secret_name
-  db_secret_arn = module.rds.db_secret_arn
-  db_endpoint = module.rds.db_endpoint
-  db_name = module.rds.db_name
-  db_instance_arn = module.rds.flytrap_db_arn
+  source               = "./modules/lambda"
+  vpc_id               = module.vpc.vpc_id
+  db_secret_name       = module.rds.db_secret_name
+  db_secret_arn        = module.rds.db_secret_arn
+  db_endpoint          = module.rds.db_endpoint
+  db_name              = module.rds.db_name
+  db_instance_arn      = module.rds.flytrap_db_arn
   private_subnet_cidrs = module.vpc.private_subnet_cidrs
-  private_subnet_ids = module.vpc.private_subnet_ids
-  sqs_queue_arn = module.sqs.sqs_queue_arn
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  sqs_queue_arn        = module.sqs_provision.sqs_queue_arn
 }
 
 module "ec2" {
